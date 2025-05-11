b475df6fcd5b4bd42f7198cc1769638405425510
Liam-Weitzel <liam.weitzel@gmail.com>
2025-05-11 09:42:37 +0200

    Update 1550

--- 1550.cpp
+++ 1550.cpp
@@ -1,7 +1,20 @@
 #include <vector>
 #include <iostream>
 
-class Solution { // Jul 01, 2024
+class Solution { // May 11, 2025
+public:
+  bool threeConsecutiveOdds(std::vector<int>& arr) {
+    int count = 0;
+    for(int i : arr) {
+      if(i % 2 != 0) count++;
+      else count = 0;
+      if(count == 3) return true;
+    }
+    return false;
+  }
+};
+
+class Solution2 { // Jul 01, 2024
 public:
   bool threeConsecutiveOdds(std::vector<int>& arr) {
     int oddCount = 0;
