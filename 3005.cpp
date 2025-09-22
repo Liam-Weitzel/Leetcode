81015503e2ad5ea4dac5d4f77002d5c808ea7dbc
Liam-Weitzel <liam.weitzel@gmail.com>
2025-09-22 07:49:58 +0200

    Update 3005

--- 3005.cpp
+++ 3005.cpp
@@ -1,9 +1,30 @@
+#include <algorithm>
 #include <map>
 #include <memory>
 #include <iostream>
+#include <unordered_map>
 #include <vector>
 
-class Solution { // Mar 08, 2024
+class Solution { // Sep 22, 2025
+public:
+  int maxFrequencyElements(std::vector<int>& nums) {
+    std::unordered_map<int, int> freqMap;
+    for(int i : nums) freqMap[i]++;
+    int max = std::max_element(
+      freqMap.begin(), freqMap.end(),
+      [](const auto &a, const auto &b) {
+        return a.second < b.second;
+      }
+    )->second;
+    int res = 0;
+    for(std::pair<int, int> i : freqMap) {
+      if(i.second == max) res += max;
+    }
+    return res;
+  }
+};
+
+class Solution2 { // Mar 08, 2024
 public:
   int maxFrequencyElements(std::vector<int>& nums) {
     std::map<int, int> frequencyMap;
--- README.md
+++ README.md
@@ -2548,7 +2548,7 @@
 |🟡|[2962. Count Subarrays Wh](2962.cpp)|🟢 Mar 29, 2024|🟢 Apr 29, 2025||||
 |🔴|[2999. Count the Number o]()|🔴 Apr 10, 2025|||||
 |🟢|[3000. Maximum Area of Lo](3000.cpp)|🟢 Aug 26, 2025|||||
-|🟢|[3005. Count Elements Wit](3005.cpp)|🟢 Mar 08, 2024|||||
+|🟢|[3005. Count Elements Wit](3005.cpp)|🟢 Mar 08, 2024|🟢 Sep 22, 2025||||
 |🟡|[3016. Minimum Number of ](3016.java)|🟢 Aug 06, 2024|||||
 |🟢|[3024. Type of Triangle](3024.cpp)|🟢 May 19, 2025|||||
 |🟢|[3136. Valid Word](3136.cpp)|🟢 Jul 15, 2025|||||
