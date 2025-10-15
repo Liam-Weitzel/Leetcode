04d965066c503ed270f351017272a29a41131390
Liam-Weitzel <liam.weitzel@gmail.com>
2025-10-15 08:50:31 +0200

    Update 3350

--- 3350.cpp
+++ 3350.cpp
@@ -1,6 +1,38 @@
-class Solution {
+#include <vector>
+#include <set>
+
+class Solution { // Oct 15, 2025
+private:
+  int isSequenceStrictlyIncreasing(std::vector<int>& nums, int left, int right) {
+    for(int j = left+1; j <= right; j++) {
+      if(nums[j] > nums[j-1]) {
+        continue;
+      } else {
+        return false;
+      }
+    }
+    return true;
+  }
+  bool hasIncreasingSubarrays(std::vector<int>& nums, int k) {
+    if(nums.size() < k*2) return false;
+    std::set<int> ranges;
+    int left = 0;
+    for(int right = k-1; right < nums.size(); right++) {
+      bool increasing = isSequenceStrictlyIncreasing(nums, left, right);
+      if(increasing) ranges.insert(left);
+      left++;
+    }
+    for(int i = 0; i < nums.size()-k; i++) {
+      if(ranges.find(i) != ranges.end() && ranges.find(i+k) != ranges.end()) return true;
+    }
+    return false;
+  }
 public:
   int maxIncreasingSubarrays(std::vector<int>& nums) {
+    int n = nums.size();
+    for (int k = n / 2; k >= 1; --k) {
+      if (hasIncreasingSubarrays(nums, k)) return k;
+    }
     return 0;
   }
 };
--- README.md
+++ README.md
@@ -2565,6 +2565,7 @@
 |🟢|[3330. Find the Original ](3330.cpp)|🟢 Jul 01, 2025|||||
 |🟡|[3335. Total Characters i](3335.cpp)|🟢 May 13, 2025|||||
 |🟢|[3349. Adjacent Increasin](3349.cpp)|🟡 Oct 14, 2025|||||
+|🟡|[3350. Adjacent Increasin](3350.cpp)|🟠 Oct 15, 2025|||||
 |🟡|[3355. Zero Array Transfo](3355.cpp)|🟢 May 20, 2025|||||
 |🟡|[3362. Zero Array Transfo](3362.cpp)|🔴 May 22, 2025|||||
 |🟡|[3372. Maximize the Numbe](3372.cpp)|🟢 May 28, 2025|||||
